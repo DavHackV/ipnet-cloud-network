026f3e08ef509d779dc019298a6af663d7952fdc
DavHackV <ekrameauxdavidkpoti@gmail.com>
2026-03-13 09:20:58 +0000

    add contact form

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Railway donne un port automatiquement
 const PORT = process.env.PORT || 3000;
 
@@ -12,6 +15,21 @@ app.use(express.static(__dirname));
 // route principale
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/contact', (req, res) => {
+
+    const name = req.body.name;
+    const email = req.body.email;
+    const message = req.body.message;
+
+    console.log("New Contact Message");
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Message:", message);
+
+    res.send("Message received successfully!");
+
 });
 
 // démarrer le serveur
